21a239605e195caffe359b0125f8a3dec89d3618
permalik <permalik@protonmail.com>
2025-08-11 23:04:03 -0500

    feat: init

---
+++ zed/blark.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title zed: blark
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Lark and Blue
+
+zed /Users/tymalik/Docs/Git/lark-web
+zed /Users/tymalik/Docs/Git/lark-bff --add
+zed /Users/tymalik/Docs/Git/lark-api --add
+zed /Users/tymalik/Docs/Git/lark-preprocess --add
+zed /Users/tymalik/Docs/Git/lark-dispatch --add
+zed /Users/tymalik/Docs/Git/lark-llm --add
+zed /Users/tymalik/Docs/Git/lark-postprocess --add
+zed /Users/tymalik/Docs/Git/lark-feedback --add
+zed /Users/tymalik/Docs/Git/lark-saga-orchestrator --add
+zed /Users/tymalik/Docs/Git/lark-saga-bus-consumer --add
+zed /Users/tymalik/Docs/Git/lark-saga-bus-producer --add
+zed /Users/tymalik/Docs/Git/lark-sidecar --add
+zed /Users/tymalik/Docs/Git/blue-core --add
+zed /Users/tymalik/Docs/Git/blue-write --add
+zed /Users/tymalik/Docs/Git/blue-rust --add
+zed /Users/tymalik/Docs/Git/blue-api --add
+zed /Users/tymalik/Docs/Git/blue-web --add
